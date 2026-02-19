887b49d6c50d0eeb05f8b1ffe22d2e3176f48b27
travsiamcrm <info@travsiam.com>
2026-02-19 15:59:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
-
 const app = express();
 
+// Railway dynamic port
+const PORT = process.env.PORT || 8080;
+
 app.get("/", (req, res) => {
-  res.send("Travsiam CRM is Live 🚀");
+  res.send("Travsiam CRM Server Running Successfully 🚀");
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
