bb555d5da2bdba5422b79cd502e157a7b18ea7e8
travsiamcrm <info@travsiam.com>
2026-02-19 15:45:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Travsiam CRM is Live 🚀");
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
