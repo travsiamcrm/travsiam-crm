7330b1f86a5a5fe1fb06a292709eca40e42f9911
travsiamcrm <info@travsiam.com>
2026-02-19 16:12:06 +0530

    Final port fix

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
-
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Travsiam CRM Live 🚀");
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
